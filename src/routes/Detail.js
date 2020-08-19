c513debb5d4a77ff36d3b174009ca01ceef73a02
ravi <artiveloper@gmail.com>
2020-08-19 18:14:06 +0900

    #1.2 GET MOVIE Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
+import {gql} from 'apollo-boost'
+import {useQuery} from '@apollo/react-hooks'
+
+const GET_MOVIE = gql`
+    query getMovie($id: Int!) {
+        movie(id: $id) {
+            id
+            title
+            medium_cover_image
+            description_intro
+        }
+    }
+`
 
 const Detail = () => {
-    return (
-        <div>detail</div>
+
+    let {id} = useParams()
+    id = parseInt(id)
+
+    const {loading, error, data} = useQuery(
+        GET_MOVIE, {
+            variables: {id}
+        }
     )
+
+    if (loading) {
+        return "loading"
+    }
+
+    if(data && data.movie) {
+        return data.movie.title
+    }
 }
 
 export default Detail
